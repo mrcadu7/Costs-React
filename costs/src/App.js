4573ca27fa21df57ec6c9a6abe86b3e13e177cdd
Cadú <139708523+mrcadu7@users.noreply.github.com>
2024-02-26 19:19:39 -0300

    Add: API, Pseudo-banco de dados, Dashboard

--- costs/src/App.js
+++ costs/src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
@@ -20,6 +21,7 @@ function App() {
         <Route path="/company" element={<Container customClass="min-height"><Company/></Container>} />
         <Route path="/contact" element={<Container customClass="min-height"><Contact/></Container>} />
         <Route path="/newproject" element={<Container customClass="min-height"><NewProject/></Container>} />
+        <Route path="/project/:id" element={<Container customClass="min-height"><Project/></Container>} />
       </Routes>
       <Footer/>
     </Router>
